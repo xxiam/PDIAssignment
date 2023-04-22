a1d608b5eda0b669d0259a5fd39f9fa6da3c4787
jhaanrei@gmail.com <20948685@student.curtin.edu.au>
2023-04-22 19:42:01 +0800

    first main commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+Jhon Gonzales - Programming Desgin and Implementation
+ */
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("=== Running Main.java ===");
+        String[] data = FileIO.parseCsv("./csvFiles/test.csv");
+    }
+}
